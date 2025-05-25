cc8b21be45ded411bd6c809ac4ba46e37f8cf2e8
Sidharth Surya <imsidharthsurya@gmail.com>
2025-05-25 11:57:32 +0530

    initial commit shadcn added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="m-10">
+      <p>Hello World</p>
+      <Button>Sign In</Button>
+    </div>
+  );
+}
+
+export default App;
